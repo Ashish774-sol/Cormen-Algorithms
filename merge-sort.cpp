b34bb18b9df1cd2e746aa8fdea11a3f86ae4a09c
Avijit Pandey <avijeetpandey87@gmail.com>
2020-09-11 17:10:56 +0530

    completed merge sort

--- merge-sort.cpp
+++ merge-sort.cpp
@@ -1,67 +1,58 @@
-//CLRS Merge Sort implementation: currently incomplete
-
 #include <iostream>
-#include <vector>
 
-void merge(std::vector<int> &arr, int p, int q, int r)
-{
-	int n1 = (q - p) + 1;
-	int n2 = (r - q);
-	int i{0}, j{0};
-	std::vector<int> left(n1);
-	std::vector<int> right(n2);
-
-	for(i = 0; i < n1; i++)
-		left[i] = arr[i + p];
-	for(j = 0; j < n2; j++)
-		right[j] = arr[j + q];
-	i = 0;
-	j = 0;
-	int k = p;
-	while(i < n1 && j < n2)
-	{
-		if(left[i] <= right[j])
-			arr[k++] = left[i++];
-		else
-			arr[k++] = right[j++];
+void merge(int *arr, int l, int m, int r){
+	int left_size = m - l + 1;
+	int right_size = r - m;
+	int *left = new int[left_size];
+	int *right = new int[right_size];
+	for(int i{0}; i < left_size; i++){
+		left[i] = arr[i + l];
 	}
-
-    while(i < n1)
-    	arr[k++] = left[i++];
-  
-    while(j < n2)
-    	arr[k++] = right[j++];
-}
-
-void mergeSort(std::vector<int> &arr, int p, int r)
-{
-	if(p < r)
-	{
-		int q = p + (r - 1) / 2;
-		mergeSort(arr, p, q);
-		mergeSort(arr, q + 1, r);
-		merge(arr, p, q, r);
+	for(int i{0}; i < right_size; i++){
+		right[i] = arr[m + i + 1];
+	}
+	int i = 0;
+	int j = 0;
+	int k = l;
+	while(i < left_size and j < right_size){
+		if(left[i] <= right[j]){
+			arr[k] = left[i];
+			i++;
+		}
+		else{
+			arr[k] = right[j];
+			j++;
+		}
+		k++;
+	}
+	while(i < left_size){
+		arr[k] = left[i];
+		i++;
+		k++;
+	}
+	while(j < right_size){
+		arr[k] = right[j];
+		j++;
+		k++;
 	}
 }
-int main()
-{
-	int n{0};
-	std::cout << "Enter the size of the array: ";
-	std::cin >> n;
-	std::cout << "Enter the array: ";
-	std::vector<int> arr(n);
-	for(int i{0}; i < n; i++)
-		std::cin >> arr[i];
 
-	std::cout << "Array before Sorting\n";
-	for(auto &element: arr)
-		std::cout << element << ' ';
+void merge_sort(int *arr, int l, int r){
+	if(l < r){
+		int m = (l+r)/2;
+		merge_sort(arr, l, m);
+		merge_sort(arr, m+1, r);
+		merge(arr, l, m , r);
+	}
+}
 
-	mergeSort(arr, 0, int(arr.size() - 1));
-
-	std::cout << "\nArray After Sorting:\n";
-	for(auto &element: arr)
-		std::cout << element << ' ';
+int main(){
+	int arr[10] {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+	int size = 10;
+	merge_sort(arr, 0, size - 1);
+	for(int i{0}; i < size; i++){
+		std::cout << arr[i] << "    ";
+	}
 	std::cout << std::endl;
 	return 0;
 }
